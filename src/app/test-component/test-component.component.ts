1ff04f8e33f931d7aaa1de321b32a5467b1a0764
milos01 <milosa942@gmail.com>
2017-08-20 23:45:38 +0200

    New test component

---
+++ src/app/test-component/test-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./test-component.component.css']
+})
+export class TestComponentComponent {
+  firstName: string = 'Milos';
+  lastName: string = 'Andric';
+}
